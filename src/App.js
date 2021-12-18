b56fa6772ac870188ff6899c7a6d29ad47181ad0
Sergey <primosudov@gmail.com>
2021-12-18 16:47:28 +0400

    add infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import React from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 const accesKey = 'BQRNxMuLlUE9XqIgSxYeqv90mRrqANvFsNFVWU7ymTk'
 function App() {
   const [images, setImages] = React.useState([]);
+  const [page, setPage] = React.useState(1);
   React.useEffect(()=>{
-    fetch(`https://api.unsplash.com/photos?client_id=${accesKey}`)
+    getPhotos()
+  },[page])
+
+  function getPhotos() {
+    fetch(`https://api.unsplash.com/photos?client_id=${accesKey}&page=${page}`)
     .then(res => res.json())
     .then(data => {
-      setImages(data)
+      setImages(images => [...images, ...data])
     })
-  },[])
+  }
   return (
     <div className="App">
       <h1>Unplash Image Gallery!</h1>
@@ -20,16 +27,37 @@ function App() {
         <button>Search</button>
       </form>
 
-      <div className='image-grid'>
+      <InfiniteScroll
+  dataLength={images.length} //This is important field to render the next data
+  next={() => {
+    setPage(page => page +1)
+    getPhotos()
+  }}
+  hasMore={true}
+  loader={<h4>Loading...</h4>}
+  endMessage={
+    <p style={{ textAlign: 'center' }}>
+      <b>Yay! You have seen it all</b>
+    </p>
+  }
+>
+<div className='image-grid'>
         {images.map((image, index) => {
           return (
-            <div className='image' key={index}>
+            <a className='image' 
+            key={index} 
+            href={image.links.html}
+            target="_blank"
+            rel='nooper noreferrer'
+            >
             <img src={image.urls.regular}
              alt={image.alt_description}/>
-          </div>
+          </a>
           )
         })}
       </div>
+</InfiniteScroll>
+      
     </div>
   );
 }
